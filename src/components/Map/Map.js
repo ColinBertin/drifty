0777eb29bfbcef5e394ddeb6ef4735b271fbbcd2
Colin Bertin <colinbertin12@gmail.com>
2022-05-26 15:27:33 +0900

    user location gps if user enable is gps

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,49 +1,63 @@
-import React, { Component, useState } from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet'
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-import './Map.css';
+import icon from "./constants";
 
-class Map extends Component {
-  render() {
+// import './Map.css';
 
-    function LocationMarker() {
-      const [position, setPosition] = useState(null)
-      const map = useMapEvents({
-        click() {
-          map.locate()
-        },
-        locationfound(e) {
-          setPosition(e.latlng)
-          map.flyTo(e.latlng, map.getZoom())
-        },
-      })
+// Tokyo coordinate
+const coords = {
+  latitude: 35.652832,
+  longitude: 139.839478
+}
 
-      return position === null ? null : (
-        <Marker position={position}>
-          <Popup>You are here</Popup>
-        </Marker>
-      )
-    }
+export default function Map() {
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const [bbox, setBbox] = useState([]);
 
-    // Tokyo coordinate
-    const coords = {
-      latitude: 35.652832,
-      longitude: 139.839478
-    }
+    const map = useMap();
+
+    useEffect(() => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+        const radius = e.accuracy;
+        const circle = L.circle(e.latlng, radius);
+        circle.addTo(map);
+        setBbox(e.bounds.toBBoxString().split(","));
+      });
+    }, [map]);
+
+    return position === null ? null : (
+      <Marker position={position} icon={icon}>
+        <Popup>
+          You are here. <br />
+          Map bbox: <br />
+          <b>Southwest lng</b>: {bbox[0]} <br />
+          <b>Southwest lat</b>: {bbox[1]} <br />
+          <b>Northeast lng</b>: {bbox[2]} <br />
+          <b>Northeast lat</b>: {bbox[3]}
+        </Popup>
+      </Marker>
+    );
+  }
+
 
     return (
       <MapContainer
-        center={{ lat: 51.505, lng: -0.09 }}
-        zoom={8}
-        scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <LocationMarker />
-      </MapContainer>
+      center={[coords.latitude, coords.longitude]}
+      zoom={8}
+      scrollWheelZoom
+      style={{ height: "100vh" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <LocationMarker /> */}
+    </MapContainer>
     );
-  }
-}
-
-export default Map;
+};
